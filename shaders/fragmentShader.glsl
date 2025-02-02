775e5bc2180c8cb0233e66c751efe61f81d55dbb
Nick Hallstrom <nick@trycrew.com>
2025-02-01 18:51:45 -0700

    Better colors

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -31,16 +31,15 @@ float noise(vec2 st) {
 void main() {
   vec3 lightDirection = normalize(vec3(1.0, 1.0, 1.0));
   float lightIntensity = max(dot(vNormal, lightDirection), 0.0);
-  float ambientIntensity = 0.0;
   vec4 textureColor = texture2D(uTexture, vUv);
-  
-  // float brightness = ambientIntensity + lightIntensity + noiseValue;
 
-  float noiseValue = (noise(vUv * 2.0) + uProgress * 0.6 - 0.3) * 30.0;
-  float newNoiseValue = 1.0 - min(1.0, max(0.0, noiseValue));
+  float noiseValue = (noise(vUv * 3.0) + uProgress * 0.6 - 0.3) * 30.0;
+  float newNoiseValue = 1.0 - clamp(noiseValue, 0.0, 1.0);
   
-  if (newNoiseValue > 0.0 && newNoiseValue < 0.8) {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, textureColor.a);
+  if (newNoiseValue > 0.0 && newNoiseValue < 0.2) {
+    float t = smoothstep(0.0, 0.2, newNoiseValue);
+    vec3 interpolatedColor = mix(vec3(1.0, 0.6, 0.6), vec3(1.0, 0.1, 0.1), t);
+    gl_FragColor = vec4(interpolatedColor, textureColor.a);
   } else if (newNoiseValue == 0.0) {
     gl_FragColor = vec4(textureColor.rgb, 0.0);
   } else  {
